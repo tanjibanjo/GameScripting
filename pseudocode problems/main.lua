52ecdfe659a28bdabf6728caa47c39097940093c
tanjibanjo <lanepollock1@gmail.com>
2025-06-19 13:18:35 -0500

    next problem

--- pseudocode problems/main.lua
+++ pseudocode problems/main.lua
@@ -1,7 +1,9 @@
 require("AddTwoNums");
 require("AddManyNums");
 require("ConvertToUpper");
+require("GenerateRandomNum");
 
 --capitalizeInput(); --using ConvertToUpper
 --printTwoNumSum(); --uses AddTwoNums
-printUserSum(); --using AddManyNums
+--printUserSum(); --using AddManyNums
+--GenerateRandomUserNumber();
