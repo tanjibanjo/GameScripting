692f7cae152dc10402f42238a99260ec32b6c63a
tanjibanjo <lanepollock1@gmail.com>
2025-07-08 17:42:15 -0500

    Create main.py

---
+++ Python/TextBasedGame/main.py
@@ -0,0 +1,12 @@
+# Lane Pollock
+# Game Scripting
+# Text-Based Adventure
+# Create a text based adventure with at least 10 prompts
+
+#__MAIN__
+def main():
+    print("Hello, world")
+
+
+if __name__ == "__main__":
+    main()
