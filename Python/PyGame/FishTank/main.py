c74031094a5965818130f6b86095ff1c60ea079a
tanjibanjo <lanepollock1@gmail.com>
2025-07-22 11:13:35 -0500

    Update main.py

--- Python/PyGame/FishTank/main.py
+++ Python/PyGame/FishTank/main.py
@@ -11,6 +11,40 @@ import random
 ###############
 pygame.init()
 
+tiles = 8
+scoreBuffer = 30
+wave = pygame.image.load("wave.png")
+water = pygame.image.load("water.png")
+waveW = wave.get_width()
+waveH = wave.get_height()
+
+#placeholder table to represent what background looks like
+v = "v" #wave
+o = "o" #water
+waterMap = {v, v, v, v, v, v, v, v,
+            o, o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o, o}
+
+#switch values to images
+for i in waterMap:
+    if i == v:
+        i = wave
+    if i == o:
+        i = water
+
+#screen
+width = tiles * waveW
+height = tiles * waveH
+size = (width, height)
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+pygame.display.set_caption("Fish Tank")
+
 ###############
 #GAME LOOP
 ###############
