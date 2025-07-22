e31f9e9e319d06b8fa51521bbc6834cf03b91836
tanjibanjo <lanepollock1@gmail.com>
2025-07-22 11:40:55 -0500

    updates

--- Python/PyGame/FishTank/main.py
+++ Python/PyGame/FishTank/main.py
@@ -21,29 +21,36 @@ waveH = wave.get_height()
 #placeholder table to represent what background looks like
 v = "v" #wave
 o = "o" #water
-waterMap = {v, v, v, v, v, v, v, v,
+waterMap = [v, v, v, v, v, v, v, v,
             o, o, o, o, o, o, o, o,
             o, o, o, o, o, o, o, o,
             o, o, o, o, o, o, o, o,
             o, o, o, o, o, o, o, o,
             o, o, o, o, o, o, o, o,
             o, o, o, o, o, o, o, o,
-            o, o, o, o, o, o, o, o}
+            o, o, o, o, o, o, o, o]
 
 #switch values to images
-for i in waterMap:
-    if i == v:
-        i = wave
-    if i == o:
-        i = water
+for i, item in enumerate(waterMap):
+    if waterMap[i] == v:
+        waterMap[i] = wave
+    if waterMap[i] == o:
+        waterMap[i] = water
 
 #screen
 width = tiles * waveW
-height = tiles * waveH
+height = tiles * waveH + scoreBuffer
 size = (width, height)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Fish Tank")
+
+#load fish
+fishies = [pygame.image.load("fish_blue.png"),
+           pygame.image.load("fish_brown.png"),
+           pygame.image.load("fish_green.png"),
+           pygame.image.load("fish_orange.png"),
+           pygame.image.load("fish_red.png"),] 
 
 ###############
 #GAME LOOP
@@ -65,6 +72,18 @@ while(not gameOver):
     ###############
     #DRAW
     ###############
+    count = 0 #count rows
+    countx = 0 #count columns
+
+    for i, item in enumerate(waterMap):
+        screen.blit(waterMap[i], (0 + (waveW *countx), scoreBuffer + (waveH *count)))
+        if (i+1) % tiles == 0:
+            count +=1
+            countx = 0
+        else:
+            countx +=1
+
+
     #flip page - render dispplay
     pygame.display.flip()
 
