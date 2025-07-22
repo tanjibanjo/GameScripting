35e0fc90387cee903d73f074d1ce29bd69c4c6a2
tanjibanjo <lanepollock1@gmail.com>
2025-07-22 10:46:15 -0500

    Update main.py

--- Python/PyGame/FishTank/main.py
+++ Python/PyGame/FishTank/main.py
@@ -1,3 +1,40 @@
 #Lane Pollock
 #Python
 #Fish tank demo in pygame
+
+import sys
+import pygame
+import random
+
+###############
+#LOAD
+###############
+pygame.init()
+
+###############
+#GAME LOOP
+###############
+gameOver = False
+while(not gameOver):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameOver = True
+
+    ###############
+    #Mouse Clicks
+    ###############
+
+    ###############
+    #UPDATE
+    ###############
+
+    ###############
+    #DRAW
+    ###############
+    #flip page - render dispplay
+    pygame.display.flip()
+
+###############
+#QUIT
+###############
+pygame.display.quit()
