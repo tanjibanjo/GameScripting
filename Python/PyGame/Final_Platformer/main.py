fa33cb80ca34fea60dd4b1e34e8e1175e22ccb1f
tanjibanjo <lanepollock1@gmail.com>
2025-07-23 13:55:29 -0500

    Update main.py

--- Python/PyGame/Final_Platformer/main.py
+++ Python/PyGame/Final_Platformer/main.py
@@ -7,18 +7,53 @@
 import pygame
 import sys
 
-#init and setup
-pygame.init()
 
-screen = pygame.display.set_mode(640,480)
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        #init and setup
+        pygame.init()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        #handle window and clock
+        icon = pygame.image.load("data/images/entities/player/idle/00.png")
+        pygame.display.set_caption("I41")
+        pygame.display.set_icon(icon)
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
 
-    #update display, locked at 60 fps
-    pygame.display.update()
-    clock.tick(60)
+        #load images
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0)) # sets color key for cloud image - 0,0,0 represents black, so black color will be made transparent
+
+        #variables
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+
+
+            self.img_pos[1] += self.movement[1] - self.movement[0] #this is to control movment through the impplicit conversion of boolean to int
+            self.screen.blit(self.img, self.img_pos)
+
+            #handle events - including k up and down
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+
+            #update display, locked at 60 fps
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
