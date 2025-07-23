df38c2a13d8bd81b6c1cc9b002aead8a6f2a481c
tanjibanjo <lanepollock1@gmail.com>
2025-07-23 13:28:15 -0500

    Update main.py

--- Python/PyGame/Final_Platformer/main.py
+++ Python/PyGame/Final_Platformer/main.py
@@ -5,8 +5,20 @@
 
 #IMPORTS
 import pygame
+import sys
 
-#init
+#init and setup
 pygame.init()
 
 screen = pygame.display.set_mode(640,480)
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #update display, locked at 60 fps
+    pygame.display.update()
+    clock.tick(60)
