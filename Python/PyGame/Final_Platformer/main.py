67f1f7fb59ad3f352027a0b745837d5d77bc9e9d
tanjibanjo <lanepollock1@gmail.com>
2025-07-23 13:23:51 -0500

    create platformer data

---
+++ Python/PyGame/Final_Platformer/main.py
@@ -0,0 +1,12 @@
+#Lane Pollock
+#Python - pygame
+#23 July 2025
+#Final project for Game Scripting - Platformer
+
+#IMPORTS
+import pygame
+
+#init
+pygame.init()
+
+screen = pygame.display.set_mode(640,480)
