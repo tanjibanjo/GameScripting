6fe6a442edbb039afefa61356ae6a0f735a3fe04
tanjibanjo <lanepollock1@gmail.com>
2025-07-23 17:19:06 -0500

    Update main.py

--- Python/PyGame/Final_Platformer/main.py
+++ Python/PyGame/Final_Platformer/main.py
@@ -24,14 +24,24 @@ class Game:
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
         self.img.set_colorkey((0, 0, 0)) # sets color key for cloud image - 0,0,0 represents black, so black color will be made transparent
 
-        #variables
+        #attributes
         self.img_pos = [160, 260]
         self.movement = [False, False]
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
 
     def run(self):
         while True:
+            #fill background
             self.screen.fill((14, 219, 248))
 
+            #collision render test
+            img_r = pygame.Rect(*self.img_pos, *self.img.get_size())
+            #img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height() )
+            #collision check
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 100, 155), self.collision_area)
 
             self.img_pos[1] += self.movement[1] - self.movement[0] #this is to control movment through the impplicit conversion of boolean to int
             self.screen.blit(self.img, self.img_pos)
@@ -41,15 +51,16 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                #movement inputs for up and down - uses keyboard keys
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                         self.movement[1] = False
 
             #update display, locked at 60 fps
